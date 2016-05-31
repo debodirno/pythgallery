770f90e524e8c58df55c2901ffc71f8e1b3d6d4a
debodirno <debodirnochandra@gmail.com>
2016-06-01 03:14:34 +0530

    Added a Binary Search program

---
+++ Binary Search/Unique_Element_Sorted_Array.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int sizeArray, midElem;
+    cout << "Enter the size of array : ";
+    cin >> sizeArray;
+
+    int arr[sizeArray], counter, key;
+
+    cout << "Enter the elements of the array : \n";
+    for(counter = 0; counter < sizeArray; counter ++)
+        cin >> arr[counter];
+
+    // to find the unique element in an array which is sorted and the other elements occur in pairs(consecutive)
+    // so normally, 0th element and 1st element are pairs, 2nd element and 3rd element are pairs, and so on.
+    // thus the middle element and the preceding/ succeeding element must be same if unique element does not lie in the first half and other conditions
+
+    int lo = 0, hi = sizeArray - 1, mid;
+    bool flag = false;
+
+    while(lo <= hi){
+        mid = (lo + hi) / 2;
+        if(arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])            // unique element(trivial)
+            break;
+        if(mid % 2 != 0){                                                   // middle element index is odd
+            if(arr[mid] == arr[mid - 1])                                    // middle element and the preceding element are same, hence the 2nd half contains he unique element
+                lo = mid + 1;
+            else                                                            // middle element and the preceding element are not same, hence the 1st half contains the unique element
+                hi = mid - 1;
+        }
+        else{                                                               // middle element index is even
+            if(arr[mid] == arr[mid + 1])                                    // middle element and the succeeding element are same, hence the 2nd half contains he unique element
+                lo = mid + 1;
+            else                                                            // middle element and the succeeding element are not same, hence the 1st half contains the unique element
+                hi = mid - 1;
+        }
+    }
+
+    cout << arr[mid] << " is a unique element in the array.\n";
+
+    return 0;
+}
